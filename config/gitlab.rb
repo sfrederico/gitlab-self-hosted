22562a317ad1f45be9e6144c17052d34518ca8ac
Samuel Frederico <sfrederico@ucs.br>
2025-11-12 16:07:09 -0300

    feat(gitlab.rb): 🚀 Adiciona configuração de porta personalizada para nginx
    * Define a porta HTTP personalizada para o nginx como 8081.

--- config/gitlab.rb
+++ config/gitlab.rb
@@ -1,6 +1,8 @@
 
 external_url 'https://gitlab.sfrederico.dev'
 
+nginx['listen_port'] = 8081 # Custom HTTP port because of nginx reverse proxy
+
 letsencrypt['enable'] = true
 letsencrypt['contact_emails'] = ['sfrederico@ucs.br']
 letsencrypt['auto_renew'] = true
